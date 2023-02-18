ba779922a8216d3b2eded736991d1cd254a1c967
Mayur <mayurbagga@hotmail.com>
2023-02-18 13:01:46 +0530

    new addresss added

--- NFT-Collection/hardhat-tutorial/constants/index.js
+++ NFT-Collection/hardhat-tutorial/constants/index.js
@@ -1,6 +1,8 @@
 // Address of the Whitelist Contract that you deployed
-const WHITELIST_CONTRACT_ADDRESS = "0x978F85b5922a83f3e51978658e3FCecbC37e9B2e";
+//const WHITELIST_CONTRACT_ADDRESS = "0x978F85b5922a83f3e51978658e3FCecbC37e9B2e";
+const WHITELIST_CONTRACT_ADDRESS = "0xbC1cf48BCDbd1cF73eE6aAce59FCB1A50379B0a7";
+
 // URL to extract Metadata for a Crypto Dev NFT
-const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+const METADATA_URL = "https://my-nft-nu.vercel.app/";
 
 module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
