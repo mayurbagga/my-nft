133b9e65e61b86364eca9df97bf15b6cf5d66f1d
Mayur <mayurbagga@hotmail.com>
2023-02-18 13:37:12 +0530

    hh again

--- NFT-Collection/hardhat-tutorial/constants/index.js
+++ NFT-Collection/hardhat-tutorial/constants/index.js
@@ -1,6 +1,6 @@
 // Address of the Whitelist Contract that you deployed
 //const WHITELIST_CONTRACT_ADDRESS = "0x978F85b5922a83f3e51978658e3FCecbC37e9B2e";
-const WHITELIST_CONTRACT_ADDRESS = "0xbC1cf48BCDbd1cF73eE6aAce59FCB1A50379B0a7";
+const WHITELIST_CONTRACT_ADDRESS = "0x201c1035E9698BB9C778119CCD22db3E416a7943";
 
 // URL to extract Metadata for a Crypto Dev NFT
 const METADATA_URL = "https://my-nft-nu.vercel.app/api/";
