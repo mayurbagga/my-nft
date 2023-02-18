d16a70255cd0037361e103336adb4cd42c7e33e1
Mayur <mayurbagga@hotmail.com>
2023-02-18 11:37:15 +0530

    initial commit

---
+++ NFT-Collection/hardhat-tutorial/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0x978F85b5922a83f3e51978658e3FCecbC37e9B2e";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
